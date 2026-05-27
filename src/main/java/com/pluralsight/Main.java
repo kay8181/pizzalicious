39714baac26e082076a237b0b809b9f1d244fc62
kay8181 <you@example.com>
2026-05-27 00:21:09 -0700

    Files

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Main {
+    public static final String RESET = "\u001B[0m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String PURPLE = "\u001B[35m";
+    public static void main(String[] args) {
+
+        System.out.println(GREEN + "__________________________" + RESET);
+        System.out.println(PURPLE + "⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯" + RESET);
+
+
+    }
+
+}
